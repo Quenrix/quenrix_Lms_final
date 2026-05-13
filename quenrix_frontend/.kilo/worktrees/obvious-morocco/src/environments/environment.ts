09be23a3b74b3c6eb0aa961829bb00b9bd069eea
Rani <raniachheylal24@navgurukul.org>
2026-05-13 14:27:04 +0530

    Admin dashboard code

--- quenrix_frontend/.kilo/worktrees/obvious-morocco/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiBaseUrl: 'http://13.61.143.129/api'
-};
--- quenrix_frontend/.kilo/worktrees/prairie-key/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'http://13.61.143.129/api'
-};
